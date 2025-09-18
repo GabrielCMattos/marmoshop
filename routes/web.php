1b733ad6d14d1aa3f433fe33e15280c81998980a
Gabriel Mattos <gabriel.casamattos1011@gmail.com>
2025-09-18 13:23:59 -0300

    Criando rotas

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\BrandsController;
 use App\Http\Controllers\CategoriesController;
+use App\Http\Controllers\UnitsController;
 use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'welcome');
@@ -18,6 +20,22 @@ Route::middleware('auth')->group(function () {
     Route::get('categories/{id}/edit', [CategoriesController::class, 'edit']);
     Route::put('categories/{id}', [CategoriesController::class, 'update']);
     Route::delete('categories/{id}', [CategoriesController::class, 'destroy']);
+
+    // Rotas de Brands
+    Route::get('brands', [BrandsController::class, 'index']);
+    Route::get('brands/create', [BrandsController::class, 'create']);
+    Route::post('brands', [BrandsController::class, 'store']);
+    Route::get('brands/{id}/edit', [BrandsController::class, 'edit']);
+    Route::put('brands/{id}', [BrandsController::class, 'update']);
+    Route::delete('brands/{id}', [BrandsController::class, 'destroy']);
+
+    // Rotas de Units
+    Route::get('units', [UnitsController::class, 'index']);
+    Route::get('units/create', [UnitsController::class, 'create']);
+    Route::post('units', [UnitsController::class, 'store']);
+    Route::get('units/{id}/edit', [UnitsController::class, 'edit']);
+    Route::put('units/{id}', [UnitsController::class, 'update']);
+    Route::delete('units/{id}', [UnitsController::class, 'destroy']);
 });
 
 
