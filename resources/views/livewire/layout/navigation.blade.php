d16af207e8259f4332417cd89e0895e9f3904573
Gabriel Mattos <gabriel.casamattos1011@gmail.com>
2025-09-17 22:20:05 -0300

    Nova loguinha

--- resources/views/livewire/layout/navigation.blade.php
+++ resources/views/livewire/layout/navigation.blade.php
@@ -24,7 +24,7 @@ new class extends Component
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
                     <a href="{{ route('dashboard') }}" wire:navigate>
-                        <img src={{ asset('marmoshoplogo.png') }} class="block h-20 w-auto fill-current text-gray-800 dark:text-gray-200" />
+                        <img src={{ asset('marmoshopminilogo.png') }} class="block h-20 w-auto fill-current text-gray-800 dark:text-gray-200" />
                     </a>
                 </div>
 
