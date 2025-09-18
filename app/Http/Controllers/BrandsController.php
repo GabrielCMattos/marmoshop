2c6e9cf8b74e8ee112e80457aa2d989d2028873c
Gabriel Mattos <gabriel.casamattos1011@gmail.com>
2025-09-17 23:25:19 -0300

    Criando base da brand controller

--- app/Http/Controllers/BrandsController.php
+++ app/Http/Controllers/BrandsController.php
@@ -2,9 +2,45 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Brand;
 use Illuminate\Http\Request;
 
 class BrandsController extends Controller
 {
-    //
+    public function index()
+    {
+        $brand = Brand::all();
+        return view('brands.index', compact('brand'));
+    }
+
+    public function create()
+    {
+        return view('brands.create');
+    }
+
+    public function store(Request $request)
+    {
+        Brand::create($request->all());
+        return redirect('brands')->with('success', 'Brand created successfully.');
+    }
+
+    public function edit($id)
+    {
+        $brand = Brand::findOrFail($id);
+        return view('brands.edit', compact('brand'));
+    }
+
+    public function update(Request $request, $id)
+    {
+        $brand = Brand::findOrFail($id);
+        $brand->update($request->all());
+        return redirect('brands')->with('success', 'Brand updated successfully.');
+    }
+
+    public function destroy($id)
+    {
+        $brand = Brand::findOrFail($id);
+        $brand->delete();
+        return redirect('brands')->with('success', 'Brand deleted successfully.');
+    }
 }
