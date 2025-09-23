d22940dd5450355e2a51efe5956da8c9eda5b087
Gabriel Mattos <gabriel.casamattos1011@gmail.com>
2025-09-23 15:56:25 -0300

    Personalizando tela home de usuário logado

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\InsumosController;
 use App\Http\Controllers\UnitsController;
 use Illuminate\Support\Facades\Route;
 
-Route::view('/', 'home');
+Route::view('/', 'home')->name('home');
 
 Route::view('dashboard', 'dashboard')->middleware(['auth', 'verified'])->name('dashboard');
 
